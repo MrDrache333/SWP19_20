a6d130545d99131b8867476ea00e2b69699ce7d4
Alexander Linke <alexander.linke@olmun.org>
2019-11-18 10:28:55 +0100

    keine verbesserungen

--- Client/src/main/java/de/uol/swp/client/SceneManager.java
+++ Client/src/main/java/de/uol/swp/client/SceneManager.java
@@ -7,6 +7,8 @@ import com.google.inject.Injector;
 import com.google.inject.assistedinject.Assisted;
 import de.uol.swp.client.auth.LoginPresenter;
 import de.uol.swp.client.auth.events.ShowLoginViewEvent;
+import de.uol.swp.client.chat.ChatService;
+import de.uol.swp.client.chat.ChatViewPresenter;
 import de.uol.swp.client.main.MainMenuPresenter;
 import de.uol.swp.client.register.RegistrationPresenter;
 import de.uol.swp.client.register.event.RegistrationCanceledEvent;
