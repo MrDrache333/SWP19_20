fc9187215131bf323ce2b05df1df87a6bc03bbbc
Keno Oelrichs Garcia <keno.oelrichs.garcia@uni-oldenburg.de>
2019-11-20 11:49:26 +0100

    Viele kleine visuelle Verbesserungen bei der Darstellung der Chatnachrichten Fehlerbehebungen  - Chatnachrichten wurden nicht richtig untereinander dargestellt  - PB wurde nicht richtig dargestellt  - PB und Name wurde an jeder Nachricht angehängt

--- Client/src/main/java/de/uol/swp/client/chat/ChatViewPresenter.java
+++ Client/src/main/java/de/uol/swp/client/chat/ChatViewPresenter.java
@@ -9,12 +9,19 @@ import de.uol.swp.common.user.UserService;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -37,8 +44,10 @@ public class ChatViewPresenter extends AbstractPresenter {
     @FXML
     private ListView messageView;
 
+
     //Liste mit formatierten Chatnachrichten
-    private static ObservableList<HBox> chatMessages;
+    private static ObservableList<VBox> chatMessages;
+    private static List<ChatMessage> chatMessageHistory = new ArrayList<>();
 
     //Services
     private static ChatService chatService;
@@ -52,6 +61,23 @@ public class ChatViewPresenter extends AbstractPresenter {
     //--------------------------------------
     // FXML
     //--------------------------------------
+    private EventHandler<KeyEvent> onKeyPressedinchatTextFieldEvent = event -> {
+        if (event.getCode() == KeyCode.ENTER) {
+            onSendChatButtonPressed();
+        }
+    };
+
+    /**
+     * On new chat message.
+     *
+     * @param msg the msg
+     */
+    public static void onNewChatMessage(NewChatMessage msg) {
+        Platform.runLater(() -> {
+            chatMessageHistory.add(msg.getMessage());
+            chatMessages.add(chatMessagetoBox(msg.getMessage()));
+        });
+    }
 
     /**
      * On send chat button pressed.
@@ -63,8 +89,6 @@ public class ChatViewPresenter extends AbstractPresenter {
         message = chatTextField.getText();
 
         if (message != "") {
-            ChatMessage newMsg = new ChatMessage(loggedInUser,"");
-            chatService.sendMessage(newMsg);
             LOG.debug("Sending message as User: "+loggedInUser.getUsername());
             ChatMessage newChatMessage = new ChatMessage(loggedInUser, message);
 
@@ -81,23 +105,12 @@ public class ChatViewPresenter extends AbstractPresenter {
     //--------------------------------------
 
     /**
-     * On new chat message.
-     *
-     * @param msg the msg
-     */
-    public static void onNewChatMessage(NewChatMessage msg) {
-        Platform.runLater(() -> {
-            chatMessages.add(chatMessagetoHBox(msg.getMessage()));
-        });
-    }
-
-    /**
      * Creates a HBox with Labels from a given ChatMessage
      *
      * @param msg the ChatMessage
      * @return a HBox with Labels
      */
-    private static HBox chatMessagetoHBox(ChatMessage msg) {
+    private static VBox chatMessagetoBox(ChatMessage msg) {
         String plainMessage = msg.getMessage();
         //Teile die Nachricht bei Leerzeichen
         String[] messagePiece = plainMessage.split(" ");
@@ -119,29 +132,77 @@ public class ChatViewPresenter extends AbstractPresenter {
         //Inhalt der HBox festlegen und mit passenden Styles versehen
         Label sender = new Label(msg.getSender().getUsername());
         Label message = new Label("  " + formatedMessage + "  ");
-        sender.setStyle("-fx-text-fill: dimgrey; -fx-font-size: 10");
-        //Wenn die Nachricht mehrere Zeilen umfasst, dann aendere den Radius der Ecken
-        message.setStyle("-fx-background-radius: " + (formatedMessage.contains("\n") ? "10" : "90") + ";-fx-background-color: dodgerblue;-fx-text-fill: white; -fx-font-size: 13");
+        sender.setStyle("-fx-text-fill: dimgrey; -fx-font-size: 12");
 
         //Je nachdem wer die Nachriht gesendet hat, diese auf der richtigen Seite darstellen
-        HBox box = new HBox();
+        VBox box = new VBox();
+        HBox hbox = new HBox();
+        ImageView pb = new ImageView();
+        pb.setImage(new Image("/images/pb_template.png"));
+        pb.setFitHeight(25);
+        pb.setFitWidth(25);
+
         if (msg.getSender().getUsername().equals(loggedInUser.getUsername())) {
+            //Wenn die Nachricht mehrere Zeilen umfasst, dann aendere den Radius der Ecken
+            message.setStyle("-fx-background-radius: " + (formatedMessage.contains("\n") ? "10" : "90") + ";-fx-background-color: #135BE5;-fx-text-fill: white; -fx-font-size: 16");
             sender.setText("Du");
             sender.setAlignment(Pos.BOTTOM_RIGHT);
-            message.setAlignment(Pos.TOP_RIGHT);
-            box.getChildren().add(message);
-            box.getChildren().add(sender);
+            message.setAlignment(Pos.BOTTOM_RIGHT);
+            message.setPadding(new Insets(3, 0, 3, 0));
+            hbox.getChildren().add(message);
             box.alignmentProperty().setValue(Pos.BOTTOM_RIGHT);
+            hbox.alignmentProperty().setValue(Pos.BOTTOM_RIGHT);
+
 
         } else {
+            //Wenn die Nachricht mehrere Zeilen umfasst, dann aendere den Radius der Ecken
+            message.setStyle("-fx-background-radius: " + (formatedMessage.contains("\n") ? "10" : "90") + ";-fx-background-color: #676767;-fx-text-fill: white; -fx-font-size: 16");
             sender.setAlignment(Pos.BOTTOM_LEFT);
-            message.setAlignment(Pos.TOP_LEFT);
-            box.getChildren().add(sender);
-            box.getChildren().add(message);
+            sender.setPadding(new Insets(0, 0, 0, 40));
+            message.setAlignment(Pos.BOTTOM_LEFT);
+            message.setPadding(new Insets(3, 0, 3, 0));
+            hbox.setSpacing(5);
+
+            //Vorrangegangene Eintraege ggf. bearbeiten
+            if (chatMessageHistory.size() > 1) {
+                String lastsender = chatMessageHistory.get(chatMessageHistory.size() - 1).getSender().getUsername();
+                if (msg.getSender().getUsername().equals(lastsender) && !loggedInUser.getUsername().equals(msg.getSender().getUsername())) {
+                    //Letzte Box abändern oder löschen?
+                    if (chatMessages.size() >= 1) {
+                        VBox tempVBox = chatMessages.get(chatMessages.size() - 1);
+                        HBox tempHBox = (HBox) tempVBox.getChildren().get(tempVBox.getChildren().size() - 1);
+                        if (tempHBox.getChildren().size() == 2) {
+                            tempHBox.getChildren().remove(0);
+                            ImageView tempImageView = new ImageView();
+                            tempImageView.setFitWidth(25);
+                            tempHBox.getChildren().add(0, tempImageView);
+                            tempVBox.getChildren().remove(tempVBox.getChildren().size() - 1);
+                            tempVBox.getChildren().add(tempHBox);
+                            replaceChatMessage(chatMessages.size() - 1, tempVBox);
+                        } else
+                            box.getChildren().add(sender);
+                    } else
+                        box.getChildren().add(sender);
+                } else {
+                    box.getChildren().add(sender);
+                }
+            } else
+                box.getChildren().add(sender);
+
+            hbox.getChildren().add(pb);
+            hbox.getChildren().add(message);
             box.alignmentProperty().setValue(Pos.BOTTOM_LEFT);
+            hbox.alignmentProperty().setValue(Pos.BOTTOM_LEFT);
         }
+        box.getChildren().add(hbox);
+        box.setPadding(new Insets(0, 0, 0, 0));
         box.setSpacing(5);
         return box;
+    }
+
+    private static void replaceChatMessage(int index, VBox box) {
+        chatMessages.remove(index);
+        chatMessages.add(index, box);
     }
 
     /**
@@ -151,8 +212,20 @@ public class ChatViewPresenter extends AbstractPresenter {
      */
     public static void updateChat(List<ChatMessage> chatMessageList) {
         Platform.runLater(() -> {
-            chatMessageList.forEach(msg -> chatMessages.add(chatMessagetoHBox(msg)));
+            chatMessageList.forEach(msg -> chatMessages.add(chatMessagetoBox(msg)));
+            chatMessageHistory.addAll(chatMessageList);
         });
+    }
+
+    /**
+     * Initialize.
+     */
+    @FXML
+    public void initialize() {
+        updateChatMessages(new ArrayList<>());
+        messageView.setItems(chatMessages);
+
+        chatTextField.setOnKeyPressed(onKeyPressedinchatTextFieldEvent);
     }
 
     //--------------------------------------
@@ -200,15 +273,6 @@ public class ChatViewPresenter extends AbstractPresenter {
         loggedInUser = user;
     }
 
-    /**
-     * Initialize.
-     */
-    @FXML
-    public void initialize() {
-        updateChatMessages(new ArrayList<>());
-        messageView.setItems(chatMessages);
-    }
-
     private void updateChatMessages(List<ChatMessage> chatMessageList) {
         // Attention: This must be done on the FX Thread!
         Platform.runLater(() -> {
@@ -217,7 +281,9 @@ public class ChatViewPresenter extends AbstractPresenter {
                 messageView.setItems(chatMessages);
             }
             chatMessages.clear();
-            chatMessageList.forEach(msg -> chatMessages.add(chatMessagetoHBox(msg)));
+            chatMessageHistory.clear();
+            chatMessageHistory.addAll(chatMessageList);
+            chatMessageList.forEach(msg -> chatMessages.add(chatMessagetoBox(msg)));
         });
     }
 
