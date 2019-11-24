0b735856be7e57e9867d7f95f646fda5e8ee11c0
Ferit <57543465+xrxrxrxr@users.noreply.github.com>
2019-11-24 13:24:05 +0100

    Merging Fehler von dev in 14 behoben.
    Es wurden 2 Dateien geändert und deswegen gab es Fehler.

--- Server/src/main/java/de/uol/swp/server/lobby/LobbyManagement.java
+++ Server/src/main/java/de/uol/swp/server/lobby/LobbyManagement.java
@@ -10,24 +10,25 @@ import java.util.*;
 
 public class LobbyManagement {
     static final Logger LOG = LogManager.getLogger(LobbyManagement.class);
+
     private Map<Integer, Lobby> lobbies = new HashMap<>();
 
     /**
-     * Lobby wird erstellt. lobbyID hat folgende Form: 067e6162-3b6f-4ae2-a171-2470b63dff00  (Beispiel) / UUID Object
-     *
      * @author Paula, Haschem, Ferit
      * @version 0.1
-     * @see <a href="http://www.javapractices.com/topic/TopicAction.do?Id=56>UUID Example Nutzung</a>
-     * @since Sprint2
+     * lobbyID hat folgende Form: 067e6162-3b6f-4ae2-a171-2470b63dff00  (Beispiel) / UUID Object
      */
 
-    public void createLobby(String name, User owner) {
+    public UUID createLobby(String name, User owner) {
         if (lobbies.containsKey(lobbies.size())) {
             throw new IllegalArgumentException("Lobby name " + name + " already exists!");
         }
+        // Erstellen der UUID für die Lobbys.
         UUID lobbyID = UUID.randomUUID();
         LOG.info("Die Lobby " + name + " hat folgende UUID erstellt bekommen: " + lobbyID);
         lobbies.put(lobbies.size(), new LobbyDTO(name, owner, lobbyID));
+
+        return lobbyID;
     }
 
     public void dropLobby(String name) {
@@ -48,4 +49,5 @@ public class LobbyManagement {
     public Collection<Lobby> getLobbies() {
         return lobbies.values();
     }
+
 }
