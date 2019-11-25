561b7b02a4940ddfc994e24dc63488ea6f4cb694
Keno Schwedler <keno.schwedler@uni-oldenburg.de>
2019-11-25 23:40:34 +0100

    Neu: - Lobbyfenster wurde nach dem Mockup erstellt - Logout-Button hat Funktionalität - Button Spielanleitung öffnet den Standardbrowser mit der Anleitung
    Known issues:
    - Beim Logout bleibt das Lobbyfenster offen
    - Andere Buttons haben keine Funktionalität
    - Chatfenster wird nicht angezeigt

--- Client/src/main/java/de/uol/swp/client/lobby/LobbyPresenter.java
+++ Client/src/main/java/de/uol/swp/client/lobby/LobbyPresenter.java
@@ -6,6 +6,10 @@ import de.uol.swp.client.AbstractPresenter;
 import de.uol.swp.client.lobby.event.ShowLobbyViewEvent;
 import de.uol.swp.server.lobby.LobbyManagement;
 import javafx.event.ActionEvent;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * @author Paula, Haschem, Ferit
@@ -15,6 +19,7 @@ import javafx.event.ActionEvent;
 public class LobbyPresenter extends AbstractPresenter {
 
     public static final String fxml = "/fxml/LobbyView.fxml";
+    private static final String url = "https://confluence.swl.informatik.uni-oldenburg.de/display/SWP2019B/Spielanleitung?preview=/126746667/126746668/Dominion%20-%20Anleitung%20-%20V1.pdf";
     private static final ShowLobbyViewEvent showLobbyViewMessage = new ShowLobbyViewEvent();
 
     public LobbyPresenter() {
@@ -28,6 +33,18 @@ public class LobbyPresenter extends AbstractPresenter {
     public void onLogoutButtonPressed(ActionEvent actionEvent) {
 
         userService.logout(loggedInUser);
+    }
+
+    public void onInstructionsButtonPressed(ActionEvent actionEvent) {
+        try {
+            Desktop.getDesktop().browse(new URI(url));
+        }
+        catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        catch (URISyntaxException e2) {
+            e2.printStackTrace();
+        }
     }
 }
 
