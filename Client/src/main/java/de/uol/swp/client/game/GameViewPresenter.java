24a4cd51e1306c5a4af9689d672b9af8244433ff
Fenja Bauer <fenja.bauer@uni-oldenburg.de>
2019-11-25 22:20:50 +0100

    Logout Button erstellt

--- Client/src/main/java/de/uol/swp/client/game/GameViewPresenter.java
+++ Client/src/main/java/de/uol/swp/client/game/GameViewPresenter.java
@@ -1,5 +1,18 @@
 package de.uol.swp.client.game;
 
-public class GameViewPresenter{
+import de.uol.swp.client.AbstractPresenter;
+import de.uol.swp.client.main.MainMenuPresenter;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class GameViewPresenter extends AbstractPresenter {
     public static final String fxml = "/fxml/GameView.fxml";
+    private static final Logger LOG = LogManager.getLogger(MainMenuPresenter.class);
+
+    @FXML
+    public void onLogoutButtonPressed(ActionEvent actionEvent) {
+        userService.logout(loggedInUser);
+    }
 }
