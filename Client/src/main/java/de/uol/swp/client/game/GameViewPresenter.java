1aba6927f740c76429a2d384ff2147e212220f70
M. Haschem <Mahmoud.haschem@uol.de>
2019-11-27 10:19:20 +0100

    Aufgeben Button Hinzugefügt!

--- Client/src/main/java/de/uol/swp/client/game/GameViewPresenter.java
+++ Client/src/main/java/de/uol/swp/client/game/GameViewPresenter.java
@@ -4,12 +4,37 @@ import de.uol.swp.client.AbstractPresenter;
 import de.uol.swp.client.main.MainMenuPresenter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Modality;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import javax.swing.*;
+import java.util.Optional;
 
 public class GameViewPresenter extends AbstractPresenter {
     public static final String fxml = "/fxml/GameView.fxml";
     private static final Logger LOG = LogManager.getLogger(MainMenuPresenter.class);
+
+    Boolean aufgeben = false;
+
+    public static void showAlert(Alert.AlertType type, String message, String title) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "");
+        alert.setResizable(false);
+        alert.initModality(Modality.APPLICATION_MODAL);
+        alert.getDialogPane().setContentText(message);
+        alert.getDialogPane().setHeaderText(title);
+        alert.show();
+        Optional<ButtonType> result = alert.showAndWait();
+        if(result.get() == ButtonType.OK){
+            System.out.println("Test1");
+        }else{
+
+        }
+
+    }
+
 
     @FXML
     public void onLogoutButtonPressed(ActionEvent actionEvent) {
@@ -18,6 +43,10 @@ public class GameViewPresenter extends AbstractPresenter {
 
     @FXML
     public void onGiveUpButtonPressed(ActionEvent actionEvent) {
+        aufgeben = true;
+
+
+        showAlert(Alert.AlertType.CONFIRMATION, "Wollen Sie wirklich Aufgeben? ", "Fehler");
 
     }
 }
