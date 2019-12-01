90a2a9f0221a7956cf6795f95c4aaa249b9a79c6
Fenja Bauer <fenja.bauer@uni-oldenburg.de>
2019-12-01 21:55:19 +0100

    Bisschen herumgespielt

--- Client/src/main/java/de/uol/swp/client/game/GameViewPresenter.java
+++ Client/src/main/java/de/uol/swp/client/game/GameViewPresenter.java
@@ -2,22 +2,34 @@ package de.uol.swp.client.game;
 
 import de.uol.swp.client.AbstractPresenter;
 import de.uol.swp.client.SceneManager;
+import de.uol.swp.client.chat.ChatViewPresenter;
 import de.uol.swp.client.main.MainMenuPresenter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.swing.*;
+import java.io.IOException;
 import java.util.Optional;
 
+/**
+ * @author fenja, hashem, marvin
+ *
+ */
+
 public class GameViewPresenter extends AbstractPresenter {
+
     public static final String fxml = "/fxml/GameView.fxml";
     private static final Logger LOG = LogManager.getLogger(MainMenuPresenter.class);
     private static SceneManager sceneManager;
+
+
 
 
     Boolean aufgeben = false;
@@ -40,7 +52,17 @@ public class GameViewPresenter extends AbstractPresenter {
         }
 
     }
-
+    @FXML
+    public void initialize() throws IOException {
+        //Neue Instanz einer ChatViewPresenter-Controller-Klasse erstellen und nötige Parameter uebergeben
+        ChatViewPresenter chatViewPresenter = new ChatViewPresenter("Game", ChatViewPresenter.THEME.Light, chatService);
+        //FXML laden
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(ChatViewPresenter.fxml));
+        //Controller der FXML setzen (Nicht in der FXML festlegen, da es immer eine eigene Instanz davon sein muss)
+        loader.setController(chatViewPresenter);
+        //Den ChatView in die chatView-Pane dieses Controllers laden
+        chatView.getChildren().add(loader.load());
+    }
 
     @FXML
     public void onLogoutButtonPressed(ActionEvent actionEvent) {
@@ -54,5 +76,7 @@ public class GameViewPresenter extends AbstractPresenter {
         showAlert(Alert.AlertType.CONFIRMATION, " ", "Möchtest du wirklich aufgeben?");
 
     }
+    @FXML
+    private Pane chatView;
 }
 
