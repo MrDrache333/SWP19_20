45d0e9e4e82357f5783cf600e5255873d8c40710
Anna Hiemenz <anna.hiemenz@uni-oldenburg.de>
2020-01-15 12:29:41 +0100

    REFACTOR: JavaDoc verbessert *Client: LobbyPresenter, Notifyer *Common: SecurityException, RetrieveAllOnlineLobbiesRequest, RequestMessage, AllOnlineUsersResponse, UserMessageSerializableTest *Server: MainMemoryBasedUserStore, UserServiceTest

--- Server/src/main/java/de/uol/swp/server/usermanagement/store/MainMemoryBasedUserStore.java
+++ Server/src/main/java/de/uol/swp/server/usermanagement/store/MainMemoryBasedUserStore.java
@@ -7,15 +7,25 @@ import de.uol.swp.common.user.UserDTO;
 import java.util.*;
 
 /**
- * This is a user store.
+ * Das ist ein UserStore.
  * <p>
- * Important: This store will never return the password of a user
+ * Wichtig: Dieser Store wird nie das Passwort eines Nutzers zurückgeben
  */
 
 public class MainMemoryBasedUserStore extends AbstractUserStore implements UserStore {
 
     private final Map<String, User> users = new HashMap<>();
 
+    /**
+     * Sucht über den Namen den Nutzer aus dem Store und überprüft ihn mithilfe des Passworts.
+     * Wenn der Nutzer existiert und die Passwörter übereinstimmen, wird dieser zurückgegeben.
+     *
+     * @param username der Username
+     * @param password das Passwort
+     * @return User oder nichts
+     * @author Marco, Julia
+     *@since Sprint0
+     */
     @Override
     public Optional<User> findUser(String username, String password) {
         User usr = users.get(username);
@@ -25,6 +35,14 @@ public class MainMemoryBasedUserStore extends AbstractUserStore implements UserS
         return Optional.empty();
     }
 
+    /**
+     * Sucht über den Namen den User aus dem Store und gibt ihn zurück, allerdings ohne Passwort.
+     *
+     * @param username der Username
+     * @return den User oder nichts
+     * @author Marco
+     * @since Sprint0
+     */
     @Override
     public Optional<User> findUser(String username) {
         User usr = users.get(username);
@@ -34,6 +52,16 @@ public class MainMemoryBasedUserStore extends AbstractUserStore implements UserS
         return Optional.empty();
     }
 
+    /**
+     * Erstellt aus den übergebenen Daten einen neuen Nutzer und speichert ihn im Store.
+     *
+     * @param username der Username
+     * @param password das Passwort
+     * @param eMail die e-Mail
+     * @return den neuen Nutzer
+     * @author Marco
+     * @since Sprint0
+     */
     @Override
     public User createUser(String username, String password, String eMail) {
         if (Strings.isNullOrEmpty(username)) {
@@ -44,6 +72,17 @@ public class MainMemoryBasedUserStore extends AbstractUserStore implements UserS
         return usr;
     }
 
+    /**
+     * Der alte Nutzer wird aus dem Store entfernt und der aus den übergeben Daten neu ertsellte dafür hinzugefügt.
+     *
+     * @param username der neue Username
+     * @param password das Passwort
+     * @param eMail die e-Mail
+     * @param oldUsername der alte Username
+     * @return den aktualisierten Nutzer
+     * @author Marco, Julia
+     * @since Sprint0
+     */
     @Override
     public User updateUser(String username, String password, String eMail, String oldUsername) {
         users.remove(oldUsername);
@@ -52,11 +91,25 @@ public class MainMemoryBasedUserStore extends AbstractUserStore implements UserS
         return usr;
     }
 
+    /**
+     * Der Nutzer wird mithilfe seines Usernamens aus dem Store gelöscht.
+     *
+     * @param username der Username
+     * @author Marco
+     * @since Sprint0
+     */
     @Override
     public void removeUser(String username) {
         users.remove(username);
     }
 
+    /**
+     * Eine Liste aller User die sich im Store befinden wird erstellt und zurückgegeben, aber ohne Passwörter.
+     *
+     * @return eine Liste aller Nutzer
+     * @author Marco
+     * @since Sprint0
+     */
     @Override
     public List<User> getAllUsers() {
         List<User> retUsers = new ArrayList<>();
