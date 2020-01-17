75937d2a42980b1d7c8031d6639ddc273a6f4f68
Alexander Linke <alexander.linke@olmun.org>
2020-01-17 12:12:42 +0100

    REFACTOR: JavaDoc überarbeitet (Klassen: client/RegistrationPresenter, client/RegisterEMailPatternTest, common/CreateLobbyMessage, common/UpdateLobbiesRequest, common/UpdateUserFailedMessage, common/Session, server/ChatManagement, server/UserManagement)

--- Common/src/main/java/de/uol/swp/common/user/message/UpdateUserFailedMessage.java
+++ Common/src/main/java/de/uol/swp/common/user/message/UpdateUserFailedMessage.java
@@ -3,18 +3,44 @@ package de.uol.swp.common.user.message;
 import de.uol.swp.common.message.AbstractServerMessage;
 import de.uol.swp.common.user.User;
 
+/**
+ * Die Nachricht, welche übermittelt wird, wenn eine Veränderung an den Nutzerdaten fehlschlägt.
+ */
 public class UpdateUserFailedMessage extends AbstractServerMessage {
 
     private User user;
     private String message;
 
+    /**
+     * Konstruktor der UpdateUserFailedMessage.
+     *
+     * @param user der User
+     * @param message die Nachricht des Fehlers
+     * @author Julia
+     */
     public UpdateUserFailedMessage(User user, String message) {
         this.user = user;
         this.message = message;
     }
 
-    public User getUser() { return user; }
+    /**
+     * Gibt den User zurück.
+     *
+     * @return der User
+     * @author Julia
+     */
+    public User getUser() {
+        return user;
+    }
 
-    public String getMessage() { return message; }
+    /**
+     * Gibt die Nachricht zurück.
+     *
+     * @return die Fehlermeldung
+     * @author Julia
+     */
+    public String getMessage() {
+        return message;
+    }
 
 }
