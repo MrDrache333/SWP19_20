0c1240a9607c133475617923e2d2fa18c324857a
Timo Siems <timo.siems@uni-oldenburg.de>
2020-02-15 12:39:32 +0100

    ADD: WebView für die Spieleanleitung * LobbyPresenter: Anpassung der "onInstructionsButtonPressed"-Methode * Hinzufügen von Imports * Hinzufügen der "startWebView"-Methode zum Anzeigen der Anleitung in einem seperaten Fenster.

--- Client/src/main/java/de/uol/swp/client/lobby/LobbyPresenter.java
+++ Client/src/main/java/de/uol/swp/client/lobby/LobbyPresenter.java
@@ -49,8 +49,6 @@ import java.util.UUID;
 public class LobbyPresenter extends AbstractPresenter {
 
     public static final String fxml = "/fxml/LobbyView.fxml";
-    private static final String url = "https://confluence.swl.informatik.uni-oldenburg.de/display/SWP2019B/Spielanleitung?preview=/126746667/126746668/Dominion%20-%20Anleitung%20-%20V1.pdf";
-
     private static final Logger LOG = LogManager.getLogger(ChatViewPresenter.class);
 
     private ChatViewPresenter chatViewPresenter;
@@ -184,14 +182,14 @@ public class LobbyPresenter extends AbstractPresenter {
      * Wird aufgerufen wenn der Spielanleitung-Button gedrückt wird.
      *
      * @param actionEvent
-     * @author Keno S, Keno O.
+     * @author Keno S, Keno O., Timo
      * @since Sprint3
      */
     @FXML
     public void onInstructionsButtonPressed(ActionEvent actionEvent) {
         try {
-            Desktop.getDesktop().browse(new URI(url));
-        } catch (IOException | URISyntaxException e1) {
+            this.lobbyService.startWebView();
+        } catch (Exception e1) {
             e1.printStackTrace();
         }
     }
@@ -526,7 +524,7 @@ public class LobbyPresenter extends AbstractPresenter {
     //--------------------------------------
 
     /**
-     * Gibt die LobyID zurück.
+     * Gibt die LobbyID zurück.
      *
      * @return die LobbyID
      * @author Darian
