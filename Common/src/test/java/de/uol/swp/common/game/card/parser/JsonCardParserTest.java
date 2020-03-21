595346c4e5bbf85fd3fe5a50a480ec9f9fd1b13d
Keno Oelrichs.Garcia <keno.oelrichs.garcia@uni-oldenburg.de>
2020-03-21 22:23:11 +0100

    Test angepasst. CHANGE:  - Deserialiser-Test entfernt, da nicht mehr benötigt

--- Common/src/test/java/de/uol/swp/common/game/card/parser/JsonCardParserTest.java
+++ Common/src/test/java/de/uol/swp/common/game/card/parser/JsonCardParserTest.java
@@ -1,13 +1,6 @@
 package de.uol.swp.common.game.card.parser;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import de.uol.swp.common.game.card.parser.action.ComplexCardAction;
-import de.uol.swp.common.game.card.parser.action.types.GetCard;
 import org.junit.jupiter.api.Test;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -20,21 +13,6 @@ class JsonCardParserTest {
         JsonCardParser parser = new JsonCardParser();
         CardPack pack = parser.loadPack("Basispack");
         assertNotNull(pack);
-    }
-
-    @Test
-    void deSerialliserComplexCardAction() {
-        GsonBuilder gsonobj = new GsonBuilder();
-
-        Gson gsonRealObj = gsonobj.create();
-        ComplexCardAction pack;
-        try {
-            pack = gsonRealObj.fromJson(new FileReader(this.getClass().getResource("/cards/parser/deserialiser/ComplexCardAction.json").toExternalForm().replace("file:", "")), GetCard.class);
-        } catch (FileNotFoundException e) {
-            pack = null;
-        }
-        assertNotNull(pack);
-
     }
 
 
