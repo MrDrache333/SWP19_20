a2d34ada0424ea7c2696b858f876af8ba2fef29d
Paulina Kowalska <paulina.kowalska@uni-oldenburg.de>
2020-03-25 12:59:12 +0100

    ADD Exception

---
+++ Common/src/main/java/de/uol/swp/common/game/exception/NotEnoughMoneyException.java
@@ -0,0 +1,14 @@
+package de.uol.swp.common.game.exception;
+
+/**
+ *
+ *
+ * @author Paula
+ * @since Sprint6
+ */
+public class NotEnoughMoneyException extends RuntimeException {
+
+    public NotEnoughMoneyException(String message) {
+        super(message);
+    }
+}
