ea98e63a6b1d05254a0213a4667235cf1d7c42e2
Devin Schlüter <devin.schlueter@uni-oldenburg.de>
2020-03-27 13:37:26 +0100

    ADDED: * Code, der beim mergen des Develop into current verloren geganen ist, wurde wieder hinzugefügt.
    Signed-off-by: Devin Schlüter <devin.schlueter@uni-oldenburg.de>

--- Server/src/main/java/de/uol/swp/server/game/Playground.java
+++ Server/src/main/java/de/uol/swp/server/game/Playground.java
@@ -3,13 +3,13 @@ package de.uol.swp.server.game;
 import com.google.inject.Inject;
 import de.uol.swp.common.game.card.ActionCard;
 import de.uol.swp.common.game.card.Card;
+import de.uol.swp.common.game.card.parser.CardPack;
+import de.uol.swp.common.game.card.parser.JsonCardParser;
 import de.uol.swp.common.game.exception.GamePhaseException;
-import de.uol.swp.common.game.messages.DrawHandMessage;
-import de.uol.swp.common.game.messages.StartActionPhaseMessage;
-import de.uol.swp.common.game.messages.StartBuyPhaseMessage;
-import de.uol.swp.common.game.messages.StartClearPhaseMessage;
+import de.uol.swp.common.game.messages.*;
 import de.uol.swp.common.game.phase.Phase;
 import de.uol.swp.common.lobby.Lobby;
+import de.uol.swp.common.message.ServerMessage;
 import de.uol.swp.common.user.User;
 import de.uol.swp.common.user.UserDTO;
 import de.uol.swp.server.game.phase.CompositePhase;
@@ -18,10 +18,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * Playground stellt das eigentliche Spielfeld dar
@@ -29,18 +26,23 @@ import java.util.UUID;
 class Playground {
 
     private static final Logger LOG = LogManager.getLogger(Playground.class);
+    private static Map<Short, Integer> cardField = new TreeMap<>();
 
     /**
      * Die Spieler
      */
     private List<Player> players = new ArrayList<>();
+    private Map<String, Integer> resultsGame = new TreeMap<>();
     private Player actualPlayer;
     private Player nextPlayer;
+    private Player latestGavedUpPlayer;
     private Phase.Type actualPhase;
     private ArrayList<Short> theIdsFromTheHand = new ArrayList<>(5);
     private GameService gameService;
     private UUID theSpecificLobbyID;
     private CompositePhase compositePhase;
+    private Timer timer = new Timer();
+    private short lobbySizeOnStart;
     private Timestamp timestamp;
 
     /**
@@ -62,36 +64,118 @@ class Playground {
         this.gameService = gameService;
         this.theSpecificLobbyID = lobby.getLobbyID();
         this.compositePhase = new CompositePhase();
-        timestamp.setTime(0);
+        this.lobbySizeOnStart = (short) lobby.getUsers().size();
+        initializeCardField();
+    }
+
+    /**
+     * Es wird das Kartenfeld übergeben.
+     *
+     * @return Das Kartenfeld, also alle Karten die auf dem Playground initalisiert sind.
+     */
+    public static Map<Short, Integer> getCardField() {
+        return cardField;
+    }
+
+    /**
+     * Methode initialisiert das Kartenfeld mit der richtigen Anzahl an Karten auf dem Feld.
+     */
+    private void initializeCardField() {
+        CardPack cardsPackField = new JsonCardParser().loadPack("Basispack");
+        for (int i = 0; i < cardsPackField.getCards().getValueCards().size(); i++) {
+            Card card = cardsPackField.getCards().getValueCards().get(i);
+            if (lobbySizeOnStart < 3) {
+                cardField.put(card.getId(), 8);
+            } else cardField.put(card.getId(), 12);
+        }
+        for (int i = 0; i < cardsPackField.getCards().getActionCards().size(); i++) {
+            Card card = cardsPackField.getCards().getActionCards().get(i);
+            cardField.put(card.getId(), 10);
+        }
+        for (int i = 0; i < cardsPackField.getCards().getReactionCards().size(); i++) {
+            Card card = cardsPackField.getCards().getActionCards().get(i);
+            cardField.put(card.getId(), 10);
+        }
+        for (int i = 0; i < cardsPackField.getCards().getMoneyCards().size(); i++) {
+            Card card = cardsPackField.getCards().getMoneyCards().get(i);
+            if (i == 0) cardField.put(card.getId(), 60);
+            else if (i == 1) cardField.put(card.getId(), 40);
+            else if (i == 2) cardField.put(card.getId(), 30);
+            else LOG.debug("Komisch: @ initializeCardField- Else Methode in 104 ausgeschlagen.... @ @ @");
+        }
     }
 
     /**
      * Initialisiert actual- und nextPlayer und aktualisiert diese, wenn ein Spieler alle Phasen durchlaufen hat.
      * Dem neuen aktuellen Spieler wird seine Hand gesendet sowie eine StartActionPhaseMessage,
      * wenn er eine Aktionskarte auf der Hand hat bzw. eine StartBuyPhaseMessage wenn nicht.
+     * Es wird zusätzlich der Timestam (vom Server) mitgeschickt
      *
-     * @author Julia
+     * @author Julia, Ferit
      * @since Sprint5
      */
     public void newTurn() {
         if (actualPlayer == null && nextPlayer == null) {
             actualPlayer = players.get(0);
             nextPlayer = players.get(1);
+            sendInitialHands();
         } else {
             //Spieler muss Clearphase durchlaufen haben
             if (actualPhase != Phase.Type.Clearphase) return;
+            if (actualPlayer != latestGavedUpPlayer) sendPlayersHand();
             sendPlayersHand();
             int index = players.indexOf(nextPlayer);
             actualPlayer = nextPlayer;
             nextPlayer = players.get(++index % players.size());
-                    }
+        }
 
         actualPhase = Phase.Type.ActionPhase;
         if (checkForActionCard()) {
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
             gameService.sendToAllPlayers(theSpecificLobbyID, new StartActionPhaseMessage(actualPlayer.getTheUserInThePlayer(), theSpecificLobbyID, timestamp));
+            phaseTimer();
         } else {
             skipCurrentPhase();
         }
+    }
+
+    /**
+     * Beendet das Spiel, versendet die gameOverMessage und löscht das Spiel im GameManagement.
+     *
+     * @param lobbyID Die Lobby in der das Spiel beendet ist.
+     * @param gameEnd Die GameOverMessage
+     */
+    public void endGame(UUID lobbyID, ServerMessage gameEnd) {
+        gameService.sendToAllPlayers(lobbyID, gameEnd);
+        gameService.dropFinishedGame(lobbyID);
+    }
+
+    /**
+     * Ein Timer skippt nach 35 Sekunden die aktuelle Phase sofern der Timer vorher nicht gecancelt worden ist. Hilfmethode endTimer ganz unten in der Kalsse. Timer wird im GameService gecancelt, wenn eine Karte innerhalb der Zeit ausgewählt worden ist.
+     */
+    public void phaseTimer() {
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                skipCurrentPhase();
+                timer.cancel();
+            }
+        }, 35000);
+    }
+
+    /**
+     * Methode, welche vom aktuellen Player die Hand versendet. Holt sich von der aktuellen Hand des Spielers die Karten und speichert die IDs dieser in einer ArrayList.
+     *
+     * @author Ferit
+     * @version 1
+     * @since Sprint5
+     */
+    public void sendPlayersHand() {
+        for (Card card : actualPlayer.getPlayerDeck().getHand()) {
+            theIdsFromTheHand.add(card.getId());
+        }
+        DrawHandMessage theHandMessage = new DrawHandMessage(theIdsFromTheHand, theSpecificLobbyID);
+        gameService.sendToSpecificPlayer(actualPlayer, theHandMessage);
     }
 
     /**
@@ -109,26 +193,12 @@ class Playground {
         if (actualPhase == Phase.Type.ActionPhase) {
             actualPhase = Phase.Type.Buyphase;
             gameService.sendToAllPlayers(theSpecificLobbyID, new StartBuyPhaseMessage(actualPlayer.getTheUserInThePlayer(), theSpecificLobbyID));
+            endTimer();
         } else {
+            actualPhase = Phase.Type.Clearphase;
             gameService.sendToAllPlayers(theSpecificLobbyID, new StartClearPhaseMessage(actualPlayer.getTheUserInThePlayer(), theSpecificLobbyID));
             compositePhase.executeClearPhase(actualPlayer);
-            actualPhase = Phase.Type.Clearphase;
         }
-    }
-
-    /**
-     * Methode, welche vom aktuellen Player die Hand versendet. Holt sich von der aktuellen Hand des Spielers die Karten und speichert die IDs dieser in einer ArrayList.
-     *
-     * @author Ferit
-     * @version 1
-     * @since Sprint5
-     */
-    public void sendPlayersHand() {
-        for (Card card : actualPlayer.getPlayerDeck().getHand()) {
-            theIdsFromTheHand.add(card.getId());
-        }
-        DrawHandMessage theHandMessage = new DrawHandMessage(theIdsFromTheHand, theSpecificLobbyID);
-        gameService.sendToSpecificPlayer(actualPlayer, theHandMessage);
     }
 
     /**
@@ -195,15 +265,76 @@ class Playground {
         this.actualPhase = actualPhase;
     }
 
+
     public Boolean playerGaveUp(UUID gameID, UserDTO secondPlayer, boolean b) {
         return true;
+    }
+
+    /**
+     * Die Methode kümmert sich um das Aufgeben des Spielers in dem spezifizierten Game/Playground.
+     *
+     * @param lobbyID
+     * @param theGivingUpUser
+     * @param wantsToGiveUp
+     * @return Ob der Spieler erfolgreich entfernt worden ist oder nicht.
+     * @author Haschem, Ferit
+     */
+    public Boolean playerGaveUp(UUID lobbyID, UserDTO theGivingUpUser, Boolean wantsToGiveUp) {
+        int thePositionInList = -1;
+        for (int i = 0; i < players.size(); i++) {
+            if (players.get(i).getPlayerName().equals(theGivingUpUser.getUsername())) {
+                thePositionInList = i;
+                break;
+            }
+        }
+        if (this.players.get(thePositionInList).getPlayerName().equals(theGivingUpUser.getUsername()) && wantsToGiveUp && lobbyID.equals(this.theSpecificLobbyID)) {
+            latestGavedUpPlayer = this.players.get(thePositionInList);
+            gameService.userGavesUpLeavesLobby(lobbyID, theGivingUpUser);
+            if (this.players.size() == 2) {
+                this.players.remove(thePositionInList);
+                GameOverMessage gameOverByGaveUp = new GameOverMessage(lobbyID, this.players.get(0).getTheUserInThePlayer(), this.players.get(0).getPlayerName(), resultsGame);
+                endGame(lobbyID, gameOverByGaveUp);
+            } else if (actualPlayer.equals(latestGavedUpPlayer)) {
+                this.players.remove(thePositionInList);
+                actualPhase = Phase.Type.Clearphase;
+                newTurn();
+            } else if (nextPlayer.equals(latestGavedUpPlayer)) {
+                if (thePositionInList < this.players.size() - 1) {
+                    nextPlayer = this.players.get(++thePositionInList);
+                    this.players.remove(thePositionInList);
+                } else {
+                    this.players.remove(thePositionInList);
+                    nextPlayer = this.players.get(0);
+                }
+            } else {
+                this.players.remove(thePositionInList);
+            }
+            return true;
+        } // TODO: Wenn Spielelogik weiter implementiert wird und ein Spieler aufgibt, Handling implementieren wie mit aufgegeben Spielern weiter umgegangen wird.
+        else {
+            return false;
+        }
+    }
+
+    /**
+     * Gibt den Spieler zurück der als letztes Aufgegeben hat.
+     *
+     * @return s.o
+     * @author Haschem, Ferit
+     * @since Sprint5
+     */
+    public Player getLatestGavedUpPlayer() {
+        return latestGavedUpPlayer;
     }
 
     public CompositePhase getCompositePhase() {
         return compositePhase;
     }
 
+    /**
+     * Beendet den Timer, sofern innerhalb der 35 Sekunden eine ActionKarte Ausgewählt worden ist.
+     */
     public void endTimer() {
-        timestamp.setTime(0);
+        timer.cancel();
     }
 }
--- Server/src/test/java/de/uol/swp/server/game/PlaygroundTest.java
+++ Server/src/test/java/de/uol/swp/server/game/PlaygroundTest.java
@@ -1,8 +1,11 @@
 package de.uol.swp.server.game;
 
+import com.google.common.eventbus.DeadEvent;
 import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
 import de.uol.swp.common.game.exception.GamePhaseException;
 import de.uol.swp.common.game.phase.Phase;
+import de.uol.swp.common.game.request.GameGiveUpRequest;
 import de.uol.swp.common.user.User;
 import de.uol.swp.common.user.UserDTO;
 import de.uol.swp.server.chat.ChatManagement;
@@ -11,10 +14,14 @@ import de.uol.swp.server.message.StartGameInternalMessage;
 import de.uol.swp.server.usermanagement.AuthenticationService;
 import de.uol.swp.server.usermanagement.UserManagement;
 import de.uol.swp.server.usermanagement.store.MainMemoryBasedUserStore;
+import de.uol.swp.server.usermanagement.store.UserStore;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,23 +29,66 @@ public class PlaygroundTest {
 
     static final User defaultOwner = new UserDTO("test1", "test1", "test1@test.de");
     static final User secondPlayer = new UserDTO("test2", "test2", "test2@test2.de");
-    static final User thirdPlayer = new UserDTO("test3", "test3", "test3@test3.de");
-    static final EventBus bus = new EventBus();
+    static final String lobbyName = "DrawHandMessageLobyTest";
+    static final String lobbyPassword = "";
     static final ChatManagement chatManagement = new ChatManagement();
     static final LobbyManagement lobbyManagement = new LobbyManagement();
     static final GameManagement gameManagement = new GameManagement(chatManagement, lobbyManagement);
-    static final AuthenticationService authenticationService = new AuthenticationService(bus, new UserManagement(new MainMemoryBasedUserStore()));
-    static final GameService gameService = new GameService(bus, gameManagement, authenticationService);
+    static final UserStore userStore = new MainMemoryBasedUserStore();
+    static final EventBus bus = new EventBus();
+    static final UserManagement userManagement = new UserManagement(userStore);
+    static final AuthenticationService authService = new AuthenticationService(bus, userManagement);
+    static final GameService gameService = new GameService(bus, gameManagement, authService);
+    static UUID id;
+    private final CountDownLatch lock = new CountDownLatch(1);
 
     static UUID gameID;
+    private Object event;
 
     @BeforeAll
     static void init() {
         gameID = lobbyManagement.createLobby("Test", "", defaultOwner);
         chatManagement.createChat(gameID.toString());
         lobbyManagement.getLobby(gameID).get().joinUser(secondPlayer);
-        lobbyManagement.getLobby(gameID).get().joinUser(thirdPlayer);
         bus.post(new StartGameInternalMessage(gameID));
+    }
+
+    /**
+     * Bei Auftreten eines DeadEvents wird dieses ausgegeben und der CountDownLatch wird um eins verringert
+     *
+     * @param e das DeadEvent
+     * @author Marco
+     * @since start
+     */
+    @Subscribe
+    void handle(DeadEvent e) {
+        this.event = e.getEvent();
+        System.out.print(e.getEvent());
+        lock.countDown();
+    }
+
+    /**
+     * Setzt vor jedem Test das aktuelle Event auf null und registriert diese Testklasse auf dem Eventbus
+     *
+     * @author Marco
+     * @since Start
+     */
+    @BeforeEach
+    void registerBus() {
+        init();
+        event = null;
+        bus.register(this);
+    }
+
+    /**
+     * Meldet diese Testklasse nach jedem Test vom Eventbus ab
+     *
+     * @author MArco
+     * @since Start
+     */
+    @AfterEach
+    void deregisterBus() {
+        bus.unregister(this);
     }
 
     /**
@@ -93,7 +143,7 @@ public class PlaygroundTest {
      */
     @Test
     void testSkipCurrentPhase() {
-        Playground playground = gameManagement.getGame(gameID).get().getPlayground();
+        Playground playground = gameManagement.getGame(id).get().getPlayground();
         playground.setActualPhase(Phase.Type.ActionPhase);
 
         playground.skipCurrentPhase();
@@ -102,5 +152,32 @@ public class PlaygroundTest {
         playground.skipCurrentPhase();
         assertEquals(Phase.Type.Clearphase, playground.getActualPhase());
         assertThrows(GamePhaseException.class, () -> playground.skipCurrentPhase());
+    }
+
+    /**
+     * Testet, ob Requests ankommen und alles richtig durchlaufen wird und ein Spieler entfernt worden ist.
+     *
+     * @author Ferit
+     * @since Sprint6
+     */
+    @Test
+    void playyerGaveUpTest() {
+        UUID spielID = gameID;
+        GameGiveUpRequest testRequest = new GameGiveUpRequest((UserDTO) secondPlayer, spielID);
+        bus.post(testRequest);
+        assertEquals(2, gameManagement.getGame(gameID).get().getPlayground().getPlayers().size());
+    }
+
+    /**
+     * Testet, ob der spezifizierte Spieler der Aufgeben will, nach Aufgabe noch im Game befindet.
+     *
+     * @author Ferit
+     * @since Sprint 6
+     */
+    @Test
+    void specificPlayerGaveUpTest() {
+        GameGiveUpRequest testRequest = new GameGiveUpRequest((UserDTO) secondPlayer, gameID);
+        bus.post(testRequest);
+        assertTrue(!gameManagement.getGame(gameID).get().getPlayground().getPlayers().contains(secondPlayer.getUsername()));
     }
 }
