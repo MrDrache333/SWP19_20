c318f9fc68de033546dff6791e40a622ccdab16f
Julia <julia.debkowski@uni-oldenburg.de>
2020-04-06 17:58:06 +0200

    FIX: gameID zur Message hinzugefügt, Variable player ist klassenweit verfügbar

--- Common/src/main/java/de/uol/swp/common/game/messages/ShowCardMessage.java
+++ Common/src/main/java/de/uol/swp/common/game/messages/ShowCardMessage.java
@@ -3,6 +3,8 @@ package de.uol.swp.common.game.messages;
 import de.uol.swp.common.game.AbstractPlayground;
 import de.uol.swp.common.message.AbstractServerMessage;
 
+import java.util.UUID;
+
 /**
  * Message, die gesendet wird, wenn eine Karte aufgedeckt werden soll.
  *
@@ -14,10 +16,12 @@ public class ShowCardMessage extends AbstractServerMessage {
     private static final long serialVersionUID = 6597124709953167310L;
     private short cardID;
     private AbstractPlayground.ZoneType zone;
+    private UUID gameID;
 
-    public ShowCardMessage(short cardID, AbstractPlayground.ZoneType zone) {
+    public ShowCardMessage(short cardID, AbstractPlayground.ZoneType zone, UUID gameID) {
         this.cardID = cardID;
         this.zone = zone;
+        this.gameID = gameID;
     }
 
     public short getCardID() {
@@ -26,5 +30,9 @@ public class ShowCardMessage extends AbstractServerMessage {
 
     public AbstractPlayground.ZoneType getZone() {
         return zone;
+    }
+
+    public UUID getGameID() {
+        return gameID;
     }
 }
