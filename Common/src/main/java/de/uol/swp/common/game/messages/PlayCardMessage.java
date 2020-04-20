872e8a9d7dee5a4660aca4892c4d8053e02b0de5
Rike Hochheiden <rike.hochheiden@uni-oldenburg.de>
2020-04-20 09:11:06 +0200

    REFACTOR: PlaycardMessage um AbstractGameMessage ergänzt

--- Common/src/main/java/de/uol/swp/common/game/messages/PlayCardMessage.java
+++ Common/src/main/java/de/uol/swp/common/game/messages/PlayCardMessage.java
@@ -1,18 +1,16 @@
 package de.uol.swp.common.game.messages;
 
+import de.uol.swp.common.game.response.AbstractGameMessage;
 import de.uol.swp.common.user.User;
 import javafx.scene.image.ImageView;
 
 import java.util.UUID;
 
-public class PlayCardMessage {
+public class PlayCardMessage extends AbstractGameMessage {
 
     /**
      * Die Message die gesendet wird, wenn eine Handkarte angeklickt wurde
      *
-     * @param lobbyID       Die ID der aktuellen Lobby
-     * @param currentUser   Der Spieler der die Request stellt
-     * @param handCardID    Die ID der angeklickten Karte
      * @param cardImage     Die ImageView der Karte
      * @param handCards     Das Array mit den ImageViews die auf der Hand sind
      * @param smallSpace    gibt an, ob die Karten zusammen gerückt sind oder nicht
@@ -22,36 +20,16 @@ public class PlayCardMessage {
      * @since Sprint 5
      */
 
-    private UUID lobbyID;
-    private User currentUser;
-    private Short handCardID;
     private ImageView cardImage;
     private int count;
     private boolean playCard;
 
-    public PlayCardMessage() {
-    }
-
     public PlayCardMessage(UUID lobbyID, User currentUser, Short handCardID, ImageView cardImage, int count,
                            boolean playCard) {
-        this.lobbyID = lobbyID;
-        this.currentUser = currentUser;
-        this.handCardID = handCardID;
+        super(lobbyID, currentUser, handCardID);
         this.cardImage = cardImage;
         this.count = count;
         this.playCard = playCard;
-    }
-
-    public UUID getLobbyID() {
-        return lobbyID;
-    }
-
-    public User getCurrentUser() {
-        return currentUser;
-    }
-
-    public Short getHandCardID() {
-        return handCardID;
     }
 
     public ImageView getCardImage() {
