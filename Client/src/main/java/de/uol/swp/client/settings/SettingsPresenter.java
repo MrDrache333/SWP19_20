18346ccbc1d77067b734296f2c2ed91308231daf
Keno Schwedler <keno.schwedler@uni-oldenburg.de>
2020-05-18 18:01:33 +0200

    CHANGE: - Der Iconpfad ist jetzt relativ.

--- Client/src/main/java/de/uol/swp/client/settings/SettingsPresenter.java
+++ Client/src/main/java/de/uol/swp/client/settings/SettingsPresenter.java
@@ -165,9 +165,9 @@ public class SettingsPresenter extends AbstractPresenter {
     public void onChatMuteToggleButtonPressed(ActionEvent actionEvent) {
         Notifyer.setMuteState(chatMuteToggleButton.isSelected());
         if (chatMuteToggleButton.isSelected())
-            chatMuteImage.setImage(new Image("file:Client/src/main/resources/images/chat_on_icon.png"));
+            chatMuteImage.setImage(new Image("images/chat_on_icon.png"));
         else
-            chatMuteImage.setImage(new Image("file:Client/src/main/resources/images/chat_off_icon.png"));
+            chatMuteImage.setImage(new Image("images/chat_off_icon.png"));
     }
 
     /**
