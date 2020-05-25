15e118a11460daaa81699e54994a663c8e58c0e2
Julia <julia.debkowski@uni-oldenburg.de>
2020-05-25 02:31:37 +0200

    REFACTORING: Code ausgebessert und aufgeräumt, JavaDoc hinzugefügt

--- Common/src/main/java/de/uol/swp/common/game/messages/UpdateCardCounterMessage.java
+++ Common/src/main/java/de/uol/swp/common/game/messages/UpdateCardCounterMessage.java
@@ -6,10 +6,16 @@ import de.uol.swp.common.user.User;
 import java.util.Map;
 import java.util.UUID;
 
+/**
+ * Message, die gesendet wird, wenn sich die Anzahl verfügbarer Karten durch Ausspielen einer Aktionskarte verändert hat
+ *
+ * @author Julia
+ * @since Sprint8
+ */
 public class UpdateCardCounterMessage extends AbstractGameMessage {
 
     private static final long serialVersionUID = 710636197251694523L;
-    Map<Short, Integer> cardCounts;
+    private Map<Short, Integer> cardCounts;
 
     public UpdateCardCounterMessage(UUID gameID, User player, Map<Short, Integer> cardCounts) {
         super(gameID, player);
