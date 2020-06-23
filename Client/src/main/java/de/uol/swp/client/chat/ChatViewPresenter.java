15ef67515968404a45c210712e4188ceee44568c
Ferit <ferit.askin@uni-oldenburg.de>
2020-06-23 23:04:30 +0200

    CHANGE: 1000 Zeichen begrenzung

--- Client/src/main/java/de/uol/swp/client/chat/ChatViewPresenter.java
+++ Client/src/main/java/de/uol/swp/client/chat/ChatViewPresenter.java
@@ -374,16 +374,16 @@ public class ChatViewPresenter extends AbstractPresenter {
         String message;
         message = chatTextField.getText();
         //Prüfe auf leere Nachricht
-        if (!message.equals("") && message.length() <= 160) {
+        if (!message.equals("") && message.length() <= 1000) {
             LOG.debug("Sende neue Chatnachricht: User= " + loggedInUser.getUsername() + " Msg= " + message + " ChatID= " + chatID);
             ChatMessage newChatMessage = new ChatMessage(loggedInUser, message);
             LOG.debug("Neue Nachricht zum Senden: " + message);
             chatTextField.clear();
             this.chatService.sendMessage(chatID, newChatMessage);
         }
-        else if (!message.equals("") && message.length() > 160) {
+        else if (!message.equals("") && message.length() > 1000) {
             Platform.runLater(() -> {
-                AlertBox alertBox = new AlertBox(Alert.AlertType.WARNING, "Bitte weniger als 160 Zeichen eingeben!", "Fehler");
+                AlertBox alertBox = new AlertBox(Alert.AlertType.WARNING, "Bitte weniger als 1000 Zeichen eingeben!", "Fehler");
             });
         }
     }
