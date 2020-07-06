6b8f21cf8e80d567154acf44cd71437a76ec45a7
Keno Oelrichs Garcia <kenoog@freenet.de>
2020-07-06 11:14:40 +0200

    Maven baut jetzt fehlerfrei. Evtl. also gefixt

--- Common/src/main/java/de/uol/swp/common/game/GameService.java
+++ Common/src/main/java/de/uol/swp/common/game/GameService.java
@@ -1,39 +1,8 @@
 package de.uol.swp.common.game;
 
-import de.uol.swp.common.user.User;
-import javafx.scene.image.ImageView;
-
-import java.util.ArrayList;
-import java.util.UUID;
-
 /**
  * Das Interface GameService.
  */
 public interface GameService {
 
-    /**
-     * Sende eine SendPlayCardRequest
-     *
-     * @param lobbyID      Die LobbyID
-     * @param loggedInUser Der eingeloggte Nutzer
-     * @param aShort       Der aShort
-     * @param cardImage    Das Bild der Karte
-     * @param handCards    Die Handkarten
-     * @param b            Der B
-     * @author Micheal Wendler
-     * @since seiner Geburt
-     */
-    void sendPlayCardRequest(UUID lobbyID, User loggedInUser, Short aShort, ImageView cardImage, ArrayList<ImageView> handCards, boolean b);
-
-    /**
-     * Send buy card request.
-     *
-     * @param lobbyID      Die LobbyID
-     * @param loggedInUser Der eingeloggte Nutzer
-     * @param valueOf      Das Value-of
-     * @param cardImage    Das Bild der Karte
-     * @author KenoO
-     * @since Sprint 4
-     */
-    void sendBuyCardRequest(UUID lobbyID, User loggedInUser, Short valueOf, ImageView cardImage);
 }
